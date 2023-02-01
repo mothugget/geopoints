bd2c6b2197a13ac013c3e1be51896869295cad16
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-02-01 18:15:20 +0100

    Add click to home

--- geopoints/components/Footer.tsx
+++ geopoints/components/Footer.tsx
@@ -5,21 +5,24 @@ import { AiOutlineUnorderedList } from 'react-icons/ai';
 import ListsSidebar from './Sidebar/ListsSidebar';
 import AddContentModal from './ContentCreation/AddContentModal';
 import { MapContext } from '../contexts/MapContext';
+import Link from 'next/link';
 
 const Footer = () => {
   const [showSidebar, setShowSidebar] = useState(false);
   const [showAddContentModal, setShowAddContentModal] =
     useState<boolean>(false);
-    
+
   return (
     <>
       {showSidebar && <ListsSidebar showSidebar={showSidebar} />}
       <footer className="h-16 z-10 bg-white">
         <div className="p-4 flex justify-between text-gray-600 fixed bottom-0 inset-x-0 z-50 bg-white">
-          <BsFillGeoFill className="w-8 h-8" />
+          <Link href="/">
+            <BsFillGeoFill className="w-8 h-8" />
+          </Link>
           <button
             onClick={() => {
-              setShowAddContentModal(!showAddContentModal)
+              setShowAddContentModal(!showAddContentModal);
             }}
           >
             <IoAddCircleOutline className="w-8 h-8" />
@@ -29,7 +32,9 @@ const Footer = () => {
             className="w-8 h-8"
           />
         </div>
-        {showAddContentModal && <AddContentModal showAddContentModal={showAddContentModal}/>}
+        {showAddContentModal && (
+          <AddContentModal showAddContentModal={showAddContentModal} />
+        )}
       </footer>
     </>
   );
