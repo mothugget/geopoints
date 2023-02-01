b4d14c33b2adbe943aedec67a3bbc53909df69c0
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-02-01 14:13:47 +0100

    Link to user profile, add tailwind component for later

--- geopoints/components/Header.tsx
+++ geopoints/components/Header.tsx
@@ -1,39 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image.js';
 import { SearchBar } from './SearchBar/SearchBar';
-import Search from './SearchBar/Search';
 import { useContext } from 'react';
 import { UserDataContext } from '../contexts/UserDataContext';
 
 const Header = () => {
   const { userData } = useContext(UserDataContext);
-  function redirectProfile() {}
 
   return (
     <div className="z-40">
       <SearchBar />
-      <Link
-        href=""
-        className="fixed
-            right-0
-            top-0
-            flex
-            z-40
-            justify-center
-            items-center
-            overflow-hidden
-            rounded-bl-lg
-            backdrop-blur-lg
-            w-16
-            h-16"
-      >
+      <Link href={`/${userData?.userName}/profile`}>
         <Image
           src={userData?.imagePath || '/fake-user-profile-pic.png'}
           alt="Profile picture"
           width={40}
           height={40}
           priority={true}
-          className="rounded-full"
+          className="rounded-full fixed right-3 top-3 z-40 backdrop-blur-lg"
         />
       </Link>
     </div>
