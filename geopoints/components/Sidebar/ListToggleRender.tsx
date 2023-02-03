52163d15521ee872783491fb52b6da6f5f79b536
Karl Fredriksson <karlpafredriksson@gmail.com>
2023-02-03 10:00:32 +0100

    fix toggle button boolean

--- geopoints/components/Sidebar/ListToggleRender.tsx
+++ geopoints/components/Sidebar/ListToggleRender.tsx
@@ -13,62 +13,72 @@ interface ListToggleProps {
 
 const ListToggle = ({ list }: ListToggleProps) => {
   const [enabled, setEnabled] = useState(false);
+  const [rerender, setRerender] = useState(false)
+
   const { user } = useUser();
   const { isError, isLoading, error, data } = useUserData(user!);
   const { displayedPoints, setDisplayedPoints } = useContext(
     DisplayedPointsContext
   );
 
-function sendPointsToMap() {
-  const allPoints = [displayedPoints, list.points]
-  if (enabled) {
-    setDisplayedPoints!(allPoints.flat())
-  } else {
-    const allPoints: Point[] = []
-    displayedPoints.forEach(point => {
-      (point.listId !== list.id) && allPoints.push(point)
-    })
-    setDisplayedPoints!(allPoints)
+  function sendPointsToMap() {
+    const allPoints = [displayedPoints, list.points]
+    if (enabled) {
+      setDisplayedPoints!(allPoints.flat())
+    } else {
+      const allPoints: Point[] = []
+      displayedPoints.forEach(point => {
+        (point.listId !== list.id) && allPoints.push(point)
+      })
+      setDisplayedPoints!(allPoints)
+    }
   }
-}
 
-  useEffect(()=>{
-    sendPointsToMap();
-  if (list.id && window.localStorage[list.id]) { setEnabled(window.localStorage[list.id])}
+  useEffect(() => {
+    console.log(list.id) //107
+    console.log(window.localStorage[list.title + list.id]) //false
+    if (list.id && window.localStorage[list.title + list.id]) {
+      console.log('HELLO I AM HERE ', list.title, ' ', list.id, ' enabled ', enabled, ' localstorage ', window.localStorage[list.title + list.id], '\n \t type ', typeof window.localStorage[list.title + list.id]) 
+      setEnabled(JSON.parse(window.localStorage[list.title + list.id]))
+      
+      setRerender(true)
+    }
   }, [])
 
 
   useEffect(() => {
-  sendPointsToMap();
-    // list.id && (window.localStorage[list.title+list.id] = enabled);
+    sendPointsToMap();
+    list.id && (window.localStorage[list.title + list.id] = enabled);
   }, [enabled])
 
- console.log(list.title, ' ', list.id, ' ', enabled)
+
 
   return (
-    <div className="mt-5 flex justify-between">
-      <Switch
-        checked={enabled}
-        onChange={setEnabled}
-        className={`${
-          enabled ? 'bg-blue-600' : 'bg-gray-200'
-        } relative inline-flex h-6 w-11 items-center rounded-full mr-6`}
-      >
-        <span
-          className={`${
-            enabled ? 'translate-x-6' : 'translate-x-1'
-          } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-        />
-      </Switch>
+    <>
+     
+        <div className="mt-5 flex justify-between">
+          <Switch
+            checked={enabled}
+            onChange={setEnabled}
+            className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'
+              } relative inline-flex h-6 w-11 items-center rounded-full mr-6`}
+          >
+            <span
+              className={`${enabled ? 'translate-x-6' : 'translate-x-1'
+                } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+            />
+          </Switch>
 
-      {data && (
-        <Link href={`/${data.userName}/lists/${list.id}`}>
-          <p className="w-32 text-sm text-gray-700 underline hover:text-blue-900">
-            {list.title}
-          </p>
-        </Link>
-      )}
-    </div>
+          {data && (
+            <Link href={`/${data.userName}/lists/${list.id}`}>
+              <p className="w-32 text-sm text-gray-700 underline hover:text-blue-900">
+                {list.title}
+              </p>
+            </Link>
+          )}
+        </div>
+      
+    </>
   );
 };
 
