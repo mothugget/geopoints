93e7f56d59f8f161e9b7781a5d21cc6d4ef55aef
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-02-03 18:59:06 +0100

    fix: bug whhen signing up

--- geopoints/components/Sidebar/GroupOfLists.tsx
+++ geopoints/components/Sidebar/GroupOfLists.tsx
@@ -8,24 +8,27 @@ interface GroupOfListsProps {
 
 const GroupOfLists = ({ title, lists }: GroupOfListsProps) => {
   return (
-    <>
-      <h3 className="mt-10 w-full text-base font-bold text-gray-800">
-        {title}
-      </h3>
-      {lists.length === 0 ? (
-        <p className="text-gray-300 italic text-sm mt-3 ml-3">
-          Nothing here yet...
-        </p>
-      ) : (
-        <ul>
-          {lists.map((list: List) => (
-            <li key={list.id}>
-              <ListToggle list={list} />
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
+    lists &&
+    title && (
+      <>
+        <h3 className="mt-10 w-full text-base font-bold text-gray-800">
+          {title}
+        </h3>
+        {lists.length === 0 ? (
+          <p className="text-gray-300 italic text-sm mt-3 ml-3">
+            Nothing here yet...
+          </p>
+        ) : (
+          <ul>
+            {lists.map((list: List) => (
+              <li key={list.id}>
+                <ListToggle list={list} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </>
+    )
   );
 };
 
