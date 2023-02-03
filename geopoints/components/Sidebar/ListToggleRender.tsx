e9f0ad00b88bd52738c29dbefa98d405b92c9d88
Karl Fredriksson <karlpafredriksson@gmail.com>
2023-02-03 10:42:28 +0100

    no render on the first point

--- geopoints/components/Sidebar/ListToggleRender.tsx
+++ geopoints/components/Sidebar/ListToggleRender.tsx
@@ -21,63 +21,78 @@ const ListToggle = ({ list }: ListToggleProps) => {
     DisplayedPointsContext
   );
 
-  function sendPointsToMap() {
+  function sendListPointsToMap() {
     const allPoints = [displayedPoints, list.points]
-    if (enabled) {
-      setDisplayedPoints!(allPoints.flat())
-    } else {
-      const allPoints: Point[] = []
-      displayedPoints.forEach(point => {
-        (point.listId !== list.id) && allPoints.push(point)
-      })
-      setDisplayedPoints!(allPoints)
-    }
+    setDisplayedPoints!(allPoints.flat())
   }
+
+  function removeListPointsFromMap() {
+    const allPoints: Point[] = []
+    displayedPoints.forEach(point => {
+      (point.listId !== list.id) && allPoints.push(point)
+    })
+    setDisplayedPoints!(allPoints)
+  }
+
 
   useEffect(() => {
     console.log(list.id) //107
-    console.log(window.localStorage[list.title + list.id]) //false
-    if (list.id && window.localStorage[list.title + list.id]) {
-      console.log('HELLO I AM HERE ', list.title, ' ', list.id, ' enabled ', enabled, ' localstorage ', window.localStorage[list.title + list.id], '\n \t type ', typeof window.localStorage[list.title + list.id]) 
-      setEnabled(JSON.parse(window.localStorage[list.title + list.id]))
-      
-      setRerender(true)
+    const toggleState = window.localStorage.getItem(list.title + list.id)
+    if (toggleState !== null) {
+      const toggleStateBool = JSON.parse(toggleState)
+      console.log(toggleStateBool) //false
+      console.log('HELLO I AM HERE ', list.title, ' ', list.id, ' enabled ', enabled, ' localstorage ', toggleStateBool, '\n \t type ', typeof toggleStateBool)
+      setEnabled(toggleStateBool)
+      if (toggleStateBool) {
+        sendListPointsToMap()
+      } else {
+        removeListPointsFromMap()
+      }
     }
   }, [])
 
 
-  useEffect(() => {
-    sendPointsToMap();
-    list.id && (window.localStorage[list.title + list.id] = enabled);
-  }, [enabled])
+  // useEffect(() => {
+  //   sendPointsToMap();
+  //   list.id && (window.localStorage[list.title + list.id] = enabled);
+  // }, [enabled])
 
-
+  function mockSetEnabled(value: any) {
+    // list.id && (window.localStorage[list.title + list.id] = value);
+    window.localStorage.setItem(list.title + list.id, value)
+    setEnabled(value);
+    if (value) {
+      sendListPointsToMap()
+    } else {
+      removeListPointsFromMap()
+    }
+  }
 
   return (
     <>
-     
-        <div className="mt-5 flex justify-between">
-          <Switch
-            checked={enabled}
-            onChange={setEnabled}
-            className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'
-              } relative inline-flex h-6 w-11 items-center rounded-full mr-6`}
-          >
-            <span
-              className={`${enabled ? 'translate-x-6' : 'translate-x-1'
-                } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-            />
-          </Switch>
 
-          {data && (
-            <Link href={`/${data.userName}/lists/${list.id}`}>
-              <p className="w-32 text-sm text-gray-700 underline hover:text-blue-900">
-                {list.title}
-              </p>
-            </Link>
-          )}
-        </div>
-      
+      <div className="mt-5 flex justify-between">
+        <Switch
+          checked={enabled}
+          onChange={mockSetEnabled}
+          className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'
+            } relative inline-flex h-6 w-11 items-center rounded-full mr-6`}
+        >
+          <span
+            className={`${enabled ? 'translate-x-6' : 'translate-x-1'
+              } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+          />
+        </Switch>
+
+        {data && (
+          <Link href={`/${data.userName}/lists/${list.id}`}>
+            <p className="w-32 text-sm text-gray-700 underline hover:text-blue-900">
+              {list.title}
+            </p>
+          </Link>
+        )}
+      </div>
+
     </>
   );
 };
