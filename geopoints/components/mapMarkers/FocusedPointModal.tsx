1887f914009ed2d1bfe1eafcf02eba7c173a16ba
Karl Fredriksson <karlpafredriksson@gmail.com>
2023-02-04 10:00:47 +0100

    it works

--- geopoints/components/mapMarkers/FocusedPointModal.tsx
+++ geopoints/components/mapMarkers/FocusedPointModal.tsx
@@ -4,14 +4,13 @@ import { MapContext } from '../../contexts/MapContext';
 
 export default function FocusedPointModal() {
     const { showPointModal, setShowPointModal, focusedPoint } = useContext(MapContext);
-
+    console.log(focusedPoint?.title)
     return (
-        <>
-            <button className='fixed top-0 left-0 h-full w-full z-20' onClick={() => setShowPointModal(!showPointModal)} />
-            {showPointModal &&
-                <div className='fixed h-52 w-52 bg-black z-50 top-0 right-0'>
-                    {focusedPoint?.title}
-                </div>}
-        </>
+        <div className={`${showPointModal ? 'translate-x-0 ' : 'translate-x-full'} fixed text-left h-full top-0 right-0 w-screen transition-all ease-in-out duration-600 z-50`}>
+            <button className=' h-full w-full z-20' onClick={() => setShowPointModal(false)} />
+            <div className=' fixed h-52 w-52 bg-black text-white z-30 top-0 right-0'>
+                {focusedPoint?.title}
+            </div>
+        </div>
     )
 }
