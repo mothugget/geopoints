b53c6f484eff906becf3892917410133ae27a298
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-02-05 22:04:14 +0100

    feat: Add Direction Services

--- geopoints/components/mapMarkers/ClickedMarkerDialog.tsx
+++ geopoints/components/mapMarkers/ClickedMarkerDialog.tsx
@@ -1,43 +1,52 @@
-import { Fragment, useContext, useState } from 'react';
+import {
+  Dispatch,
+  Fragment,
+  SetStateAction,
+  useContext,
+  useState,
+} from 'react';
 import {
   Button,
   Dialog,
   DialogHeader,
-  DialogBody,
   DialogFooter,
 } from '@material-tailwind/react';
 import { ClickedMarkerContext } from '../../contexts/ClickedMarkerContext';
+import { Point } from '../../types/types';
 
-export default function ClickedMarkerDialog() {
+interface ClickedMarkerDialog {
+  setShouldRoutesBeShown: Dispatch<SetStateAction<boolean>>;
+}
+export default function ClickedMarkerDialog({
+  setShouldRoutesBeShown,
+}: ClickedMarkerDialog) {
   const [open, setOpen] = useState(false);
-  // const [prevPointId, setPrevPointId] = useState(0);
+  const [prevClickedPoint, setPrevClickedPoint] = useState<Point | null>();
+  const { clickedPoint } = useContext(ClickedMarkerContext);
 
-  const { clickedPointId, setClickedPointId } =
-    useContext(ClickedMarkerContext);
+  if (prevClickedPoint?.id !== clickedPoint?.id) {
+    console.log('hello');
+    setPrevClickedPoint(clickedPoint);
+    setOpen(true);
+    setShouldRoutesBeShown(false);
+  }
 
-  const handleOpen = () => {
-    console.log(
-      "I'm in the modal dialog and the id of the point youclicked is: ",
-      clickedPointId
-    );
-    setOpen(!open);
-    setClickedPointId && setClickedPointId(null);
+  const handleOpen = () => setOpen(!open);
+
+  const handleShowRoute = () => {
+    setShouldRoutesBeShown((shouldRoutesBeShown) => !shouldRoutesBeShown);
+    setOpen(false);
   };
-
   return (
     <Fragment>
-      <Dialog
-        open={clickedPointId === 0 ? true : Boolean(clickedPointId)}
-        handler={handleOpen}
-        className="w-96 flex flex-col"
-      >
+      <Dialog open={open} handler={handleOpen} className="w-96 flex flex-col">
         <DialogHeader className="text-xl mt-2">Point Options:</DialogHeader>
         <DialogFooter className="flex flex-col justify-center items-cente">
           <Button
             className="my-1 w-24"
             variant="gradient"
             color="blue"
-            onClick={handleOpen}
+            onClick={handleShowRoute}
           >
             Show route
           </Button>
--- geopoints/pages/index.tsx
+++ geopoints/pages/index.tsx
@@ -1,13 +1,13 @@
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
-import { useContext } from 'react';
+import { useState } from 'react';
 import { useUserData } from '../hooks/useUserData';
-import { ClickedMarkerContext } from '../contexts/ClickedMarkerContext';
 import Map from '../components/Map';
 import Header from '../components/Header';
 import LoadingSpinner from '../components/LoadingSpinner';
 import ClickedMarkerDialog from '../components/mapMarkers/ClickedMarkerDialog';
 
 export default withPageAuthRequired(function Home() {
+  const [shouldRoutesBeShown, setShouldRoutesBeShown] = useState(false);
   const { user } = useUser();
   const { isError, isLoading, error, data } = useUserData(user!);
 
@@ -18,13 +18,13 @@ export default withPageAuthRequired(function Home() {
   if (isError && error instanceof Error) {
     return <span className="text-black">Error: {error.message}</span>;
   }
-  console.log({ data });
+
   return (
     <main className="flex flex-col h-screen justify-between bg-white">
       <Header />
       <section className="mb-auto">
-        <ClickedMarkerDialog />
-        <Map />
+        <ClickedMarkerDialog setShouldRoutesBeShown={setShouldRoutesBeShown} />
+        <Map shouldRoutesBeShown={shouldRoutesBeShown} />
       </section>
     </main>
   );
