628d38777e341f172bc31875a581983ad746ffef
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-02-05 10:06:42 +0100

    lint

--- geopoints/__tests__/components/Sidebar/Footer.test.tsx
+++ geopoints/__tests__/components/Sidebar/Footer.test.tsx
@@ -1,13 +1,12 @@
-import {render, screen} from '@testing-library/react';
-import Footer from '../../../../../../components/Footer';
-import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { render, screen } from '@testing-library/react';
+import Footer from '../../../components/Footer';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-
-describe ('Footer', () => {
+describe('Footer', () => {
   const queryClient = new QueryClient();
 
-  it('renders an image with my icon\'s url', () => {
+  it("renders an image with my icon's url", () => {
     render(
       <UserProvider>
         <QueryClientProvider client={queryClient}>
@@ -17,20 +16,19 @@ describe ('Footer', () => {
     );
 
     const image = screen.getByAltText('home logo');
-    expect(image.getAttribute("src")).toContain("geopoints-logo.png");
+    expect(image.getAttribute('src')).toContain('geopoints-logo.png');
   }),
+    it('renders a button that says NEW', () => {
+      render(
+        <UserProvider>
+          <QueryClientProvider client={queryClient}>
+            <Footer />
+          </QueryClientProvider>
+        </UserProvider>
+      );
 
-  it('renders a button that says NEW', () => {
-    render(
-      <UserProvider>
-        <QueryClientProvider client={queryClient}>
-          <Footer />
-        </QueryClientProvider>
-      </UserProvider>
-    );
-
-    const button = screen.getByText(/NEW/);
-    console.log("HEREEEEEEEEEEEE", button)
-    expect(button).toBeInTheDocument();
-  })
-})
+      const button = screen.getByText(/NEW/);
+      console.log('HEREEEEEEEEEEEE', button);
+      expect(button).toBeInTheDocument();
+    });
+});
