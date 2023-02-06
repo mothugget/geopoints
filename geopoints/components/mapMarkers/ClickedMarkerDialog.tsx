0b6e58704646e70db5640797f7262148afba7c51
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-02-06 11:30:42 +0100

    Abstract delete point hook

--- geopoints/components/mapMarkers/ClickedMarkerDialog.tsx
+++ geopoints/components/mapMarkers/ClickedMarkerDialog.tsx
@@ -13,6 +13,7 @@ import {
 } from '@material-tailwind/react';
 import { ClickedMarkerContext } from '../../contexts/ClickedMarkerContext';
 import { Point } from '../../types/types';
+// import useDeletePoint from '../../hooks/useD';
 
 interface ClickedMarkerDialog {
   setShouldRoutesBeShown: Dispatch<SetStateAction<boolean>>;
@@ -24,6 +25,7 @@ export default function ClickedMarkerDialog({
   const [prevClickedPoint, setPrevClickedPoint] = useState<Point | null>();
   const [showRemoveRouteButton, setShowRemoveRouteButton] = useState(false);
   const { clickedPoint } = useContext(ClickedMarkerContext);
+  // const mutation = useMutation((pointId: number) => deletePoint(pointId));
 
   if (prevClickedPoint?.id !== clickedPoint?.id) {
     setPrevClickedPoint(clickedPoint);
@@ -37,6 +39,11 @@ export default function ClickedMarkerDialog({
     setShouldRoutesBeShown((shouldRoutesBeShown) => !shouldRoutesBeShown);
     setOpen(false);
   };
+
+  // const handleDelete = () => {
+  //   mutation.mutate(clickedPoint?.id);
+  // };
+
   return (
     <Fragment>
       <Dialog open={open} handler={handleOpen} className="w-96 flex flex-col">
@@ -62,7 +69,7 @@ export default function ClickedMarkerDialog({
             className="my-1 w-24"
             variant="gradient"
             color="red"
-            onClick={handleOpen}
+            onClick={handleDelete}
           >
             Delete
           </Button>
