9242218416b58fd9eb0920b775c4c0ea13126078
Mark Kagan <markkagan1@gmail.com>
2023-02-07 20:20:38 +0100

    update

--- geopoints/pages/points/[pointId].tsx
+++ geopoints/pages/points/[pointId].tsx
@@ -4,18 +4,14 @@ import { Point } from "../../types/types.js";
 import PointDisplay from "../../components/PointPage/PointDisplay";
 import LoadingSpinner from "../../components/LoadingSpinner";
 import { useUser } from "@auth0/nextjs-auth0/client";
-// import { Button } from '@material-tailwind/react';
+import { Button } from '@material-tailwind/react';
 import { useRouter } from "next/router.js";
-import { Button } from "@mui/material";
 
 const prisma = new PrismaClient();
 
 function PointPage({ pointData }: { pointData: Point }) {
   const router = useRouter();
-  const user = useUser();
-  // const {data} = useUserData(user);
-  // const userName = data?.userName;
-  // console.log(userName)
+
   return (
     <>
       {pointData ? (
@@ -30,11 +26,7 @@ function PointPage({ pointData }: { pointData: Point }) {
         <LoadingSpinner />
       )}
       <Button
-        style={{
-          borderRadius: 8,
-          backgroundColor: "#399EF1",
-        }}
-        variant="contained"
+        ripple={false}
         onClick={() => {
           router.back();
         }}
@@ -45,7 +37,6 @@ function PointPage({ pointData }: { pointData: Point }) {
     </>
   );
 }
-// () => router.push(`/${userName}/lists/${pointData.listId}`);
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { pointId } = context.query;
